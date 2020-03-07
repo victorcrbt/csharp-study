723015eac7b78f8e9af427cf795c2d5c955da67e
Victor Batalha <madivcb@gmail.com>
2020-03-07 16:25:27 -0300

    initial commit

---
+++ Ch_02/SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string SchoolName;
+        string SchollAddress;
+        string SchoolCity;
+        string SchoolState;
+        string SchoolZip;
+        string PhoneNumber;
+    }
+}
