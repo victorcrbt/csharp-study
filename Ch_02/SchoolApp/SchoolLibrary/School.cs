3b1f58779bd0cae1fbaa296545155942e74e7fb2
Victor Batalha <madivcb@gmail.com>
2020-03-07 17:06:08 -0300

    Getters and setters

--- Ch_02/SchoolApp/SchoolLibrary/School.cs
+++ Ch_02/SchoolApp/SchoolLibrary/School.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SchoolLibrary {
-    class School {
-        string SchoolName;
-        string SchollAddress;
-        string SchoolCity;
-        string SchoolState;
-        string SchoolZip;
-        string PhoneNumber;
+namespace SchoolLibrary 
+{
+  class School 
+  {
+    string Name { get; set; } // This is called an auto property. Basically defines getter and setter for that property automatically.
+    string Address { get; set; }
+    string City { get; set; }
+    string State { get; set; }
+    string Zip { get; set; }
+    string PhoneNumber { get; set; }
 
-        Elementary.Volume volume;
-        HighSchool.Volume volume2;
+    string _twitterAddress;
+    string TwitterAddress 
+    {
+      // Make sure that twitter address starts with @
+      get { return _twitterAddress; }
+
+      set
+      {
+        // The value comes automatically in the context of getters and setters
+        if (value.StartsWith("@"))
+        {
+          _twitterAddress = value;
+        }
+        else
+        {
+          throw new Exception("The Twitter address must begin with and @.");
+        }
+      }
     }
+  }
 }
