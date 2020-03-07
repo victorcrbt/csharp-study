4c8d6d7778fbfc58dab006229dff6dfdaabe95e0
Victor Batalha <madivcb@gmail.com>
2020-03-07 16:49:16 -0300

    Auto properties

--- Ch_02/SchoolApp/SchoolLibrary/School.cs
+++ Ch_02/SchoolApp/SchoolLibrary/School.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SchoolLibrary
-{
-    class School
-    {
+namespace SchoolLibrary {
+    class School {
         string SchoolName;
         string SchollAddress;
         string SchoolCity;
         string SchoolState;
         string SchoolZip;
         string PhoneNumber;
+
+        Elementary.Volume volume;
+        HighSchool.Volume volume2;
     }
 }
